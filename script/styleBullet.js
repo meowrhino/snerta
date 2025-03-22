3aa42fa70c07156f94183630f33bebdeadbb84eb
manu <manuellatourf@gmail.com>
2025-03-22 20:04:43 +0100

    bullets para móvil y paddings vh para movil

--- script/styleBullet.js
+++ script/styleBullet.js
@@ -1,3 +1,4 @@
-document.querySelectorAll('ul li').forEach((li, index) => {
-    li.style.setProperty('--delay', `${index * 0.5}s`);
+// Aplica solo a los elementos de la sección correspondiente
+document.querySelectorAll('#acompanyaments ul li').forEach((li, index) => {
+    li.style.setProperty('--delay', `${index * 0.3}s`); // Ajusta el timing
 });
